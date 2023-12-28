d990e04d5f7d67c251158b6d1bd78251ec81a1df
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-12-28 14:52:33 -0300

    feat: recebe, verifica e exibe argumento recebido através da linha de comando

--- caesar.c
+++ caesar.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <locale.h>
 
-
 int main(int argc, char const *argv[])
 {
   setlocale(LC_ALL, "Portuguese_Brazil");
 
-  printf("Oláç");
+  if (argc != 2) {
+    printf("Defina através da interface de linha de comando uma única chave para a cifra de Caesar!");
+    return 1;
+  }
+
+  printf("Sua chave: %s\n", argv[1]);
+  printf("Texto simples: \n");
+  printf("Texto cifrado: \n");
 
   return 0;
 }
