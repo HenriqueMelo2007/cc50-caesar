853bc0cded90f58a2cd6730f6a3ca8e29d938c0d
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-12-31 11:58:57 -0300

    feat: cria ponteiro para armazenar texto fornecido pelo usuário

--- caesar.c
+++ caesar.c
@@ -6,13 +6,19 @@ int main(int argc, char const *argv[])
   setlocale(LC_ALL, "Portuguese_Brazil");
 
   if (argc != 2) {
-    printf("Defina através da interface de linha de comando uma única chave para a cifra de Caesar!");
+    printf("Defina atravï¿½s da interface de linha de comando uma ï¿½nica chave para a cifra de Caesar!");
     return 1;
   }
 
+  char *textoSimples;
+
   printf("Sua chave: %s\n", argv[1]);
-  printf("Texto simples: \n");
+
+  printf("Texto simples: ");
+  scanf("%s", textoSimples);
+
   printf("Texto cifrado: \n");
+  
 
   return 0;
 }
